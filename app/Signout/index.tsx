24ca67a259a5c86d59b9395ea987f11d89b9eb9f
SharmaTanishq <tanishqsharma.sharma@gmail.com>
2025-01-27 14:46:02 +0530

    optimised for mobile now

--- app/Signout/index.tsx
+++ app/Signout/index.tsx
@@ -20,8 +20,8 @@ const Signout = () => {
     const router = useRouter();
     const words = ["faster", "cheaper", "visually", "better", "easier" , "smarter"];
     return (
-    <div className='flex flex-col w-full justify-center items-center px-4 py-5'>
-        <div className='max-w-7xl p-10  flex flex-col w-full h-full '>
+    <div className='flex flex-col w-full justify-center items-center px-2 md:px-4 py-5'>
+        <div className='max-w-7xl p-4 md:p-10  flex flex-col w-full h-full '>
             
             <div className='flex flex-col h-full  justify-center items-center '>
             <div className="inline-flex items-center mb-6 gap-2 px-4 py-2 rounded-full bg-[#202632] border border-white/10 text-sm">
@@ -31,18 +31,19 @@ const Signout = () => {
             </div>  
                 
                 <h1
-                    className='text-2xl md:text-[85px] md:leading-[5rem] text-[#0F1218] leading-normal font-bold text-center p-2 overflow-hidden bg-gradient-to-r from-indigo-500 to-violet-500 bg-clip-text text-transparent'                                    
+                    className='text-4xl md:text-[85px] md:leading-[5rem] text-[#0F1218] leading-normal font-bold text-center p-2 overflow-hidden bg-gradient-to-r from-indigo-500 to-violet-500 bg-clip-text text-transparent'                                    
                     
                     >Migrate faster with
                 </h1>
-                <SparklesText text="Bridgeflow" className='text-2xl md:text-[85px] font-bold mt-10' />
-                <p className='max-w-[50ch] tracking-wide mb-1 mt-10 text-color-primary-black font-light text-[20px] text-center'>
+                <SparklesText text="Bridgeflow" className='text-5xl md:text-[85px] font-bold md:mt-10' />
+                
+                <p className='max-w-[50ch] tracking-wide md:leading-normal mb-1 mt-4 md:mt-10 text-color-primary-black font-light text-xs md:text-[20px] text-center'>
                     Build integrations faster with our AI-powered platform that connects your systems seamlessly.
                 </p>
-                <div className='flex w-1/4 justify-between mt-10'>
+                <div className='flex w-2/4 md:w-1/4 justify-between mt-4 md:mt-10'>
                     <PulsatingButton 
                        onClick={()=>router.push("/waitlist")}
-                        className='bg-gradient-to-r w-full  rounded-full from-indigo-600 to-violet-600 hover:from-indigo-500 hover:to-violet-500 text-xl font-light text-white  transition-all duration-200'
+                        className='bg-gradient-to-r w-full  rounded-full from-indigo-600 to-violet-600 hover:from-indigo-500 hover:to-violet-500 md:text-xl font-light text-white  transition-all duration-200'
                         
                     >
                         Join Waitlist
@@ -80,7 +81,7 @@ const Signout = () => {
             
         </div>
         
-        <div id='bridgeflow' className='w-full flex flex-col justify-center items-center mt-10 '>
+        <div id='bridgeflow' className='w-full flex flex-col justify-center items-center md:mt-10 '>
             <TimelineSection/>
         </div>
 
